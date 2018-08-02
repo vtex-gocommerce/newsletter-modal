0bedc1c8deabf49ee51fb2f2df271464f383dbe8
Gustavo Rosolem <guspace92@hotmail.com>
2018-08-02 18:40:38 -0300

    Add form template

--- react/adminComponents/components/pageHeader.tsx
+++ react/adminComponents/components/pageHeader.tsx
@@ -1,24 +1,41 @@
 import * as React from 'react'
-import { Breadcrumb, Tab } from 'gocommerce.styleguide'
+import { injectIntl } from 'react-intl'
+import { Breadcrumb, Tab, Button } from 'gocommerce.styleguide'
 
 interface PageHeaderProps {
   tabsConfig: any[]
   activeTab: string
   handleChangeTab: Function
+  handleSave: Function
+  handleCancel: Function
   breadcrumbConfig: any
+  intl: {
+    formatMessage({ id: string }, values?: {
+      [key: string]: string | number
+    })
+  }
+  locale: string
 }
 
 interface PageHeaderState {}
 
 class PageHeader extends React.PureComponent<PageHeaderProps, PageHeaderState> {
   render() {
-    const { tabsConfig, activeTab, handleChangeTab, breadcrumbConfig } = this.props
+    const { tabsConfig, activeTab, breadcrumbConfig, handleChangeTab, handleSave, handleCancel } = this.props
 
     return (
       <div className="fixed right-0 w-100 z-4 top-0">
         <div className="g-ml15 bg-white bb b--base-4 g-ph8">
           <div className="flex justify-between g-pv4 items-center">
             <Breadcrumb items={breadcrumbConfig} />
+            <div>
+              <Button onClick={() => handleCancel()} className="g-mr2" style="secondary">
+                {this.props.intl.formatMessage({ id: 'admin.container.cancel' })}
+              </Button>
+              <Button onClick={() => handleSave()} className="g-mr2" style="primary">
+                {this.props.intl.formatMessage({ id: 'admin.container.save' })}
+              </Button>
+            </div>
           </div>
           <div className="flex justify-between">
             <Tab list={tabsConfig} initialTab={activeTab} forceValue={activeTab} onClick={handleChangeTab} />
@@ -29,4 +46,4 @@ class PageHeader extends React.PureComponent<PageHeaderProps, PageHeaderState> {
   }
 }
 
-export default PageHeader
+export default injectIntl(PageHeader)
--- react/adminComponents/settings.tsx
+++ react/adminComponents/settings.tsx
@@ -2,11 +2,16 @@ import * as React from 'react'
 import { Query, Mutation } from 'react-apollo'
 import { FormattedMessage, injectIntl } from 'react-intl'
 import { Helmet } from 'render'
+import { WithNavigate } from 'gocommerce.gc-utils'
+import { Container, Input, Toggle, Button } from 'gocommerce.styleguide'
 import PageHeader from './components/pageHeader'
 import GetNewsletterModalConfig from './../graphql/GetNewsletterModalConfig.gql'
 import SetNewsletterModalConfig from './../graphql/SetNewsletterModalConfig.gql'
 
+const NavigateHoc = WithNavigate.HOC
+
 interface NewsletterConfigProps {
+  navigate?: Function
   intl: {
     formatMessage({ id: string }, values?: {
       [key: string]: string | number
@@ -15,10 +20,32 @@ interface NewsletterConfigProps {
   locale: string
 }
 
+@NavigateHoc()
 class NewsletterConfig extends React.Component<NewsletterConfigProps, {}> {
+  handleCancel = () => {
+    this.props.navigate({
+      to: '/admin/marketing/newsletter-modal'
+    })
+  }
+  handleSave = (e, sendMutation) => {
+    e.preventDefault();
+    sendMutation({
+      variables: {
+        config: {
+          modalTitle: '',
+          modalDescription: '',
+          modalTextSuccess: '',
+          modalEnabled: false,
+          modalShowRule: '',
+          modalShowWhen: ''
+        }
+      }
+    })
+    return false
+  }
   render() {
     const breadcrumbConfig = [
-      { title: <FormattedMessage id="newsletter-modal.admin.newsletter" />, to: 'admin/marketing/newsletter-modal' },
+      { title: <FormattedMessage id="newsletter-modal.admin.newsletter" />, to: '/admin/marketing/newsletter-modal' },
       { title: <FormattedMessage id="newsletter-modal.admin.settings" /> }
     ]
     const tabsConfig = [
@@ -27,7 +54,7 @@ class NewsletterConfig extends React.Component<NewsletterConfigProps, {}> {
       { label: <FormattedMessage id="newsletter-modal.admin.apps" />, id: 'apps' }
     ]
     return (
-      <div className="min-h-100">
+      <div className="">
         <Helmet>
           <title>{this.props.intl.formatMessage({ id: 'newsletter-modal.admin.settings.page-title' })}</title>
         </Helmet>
@@ -36,8 +63,10 @@ class NewsletterConfig extends React.Component<NewsletterConfigProps, {}> {
           tabsConfig={tabsConfig}
           activeTab={'general'}
           handleChangeTab={() => ''}
+          handleSave={this.handleSave}
+          handleCancel={this.handleCancel}
         />
-        <div className="g-mh8 g-mb8 g-mt14 g-pt8">
+        <div className="center g-mb8 g-ph8 g-pt14 g-mw8">
           <Query query={GetNewsletterModalConfig} ssr={false}>
             {({ loading: queryLoading, error: queryError, data }) => (
               <Mutation
@@ -46,10 +75,7 @@ class NewsletterConfig extends React.Component<NewsletterConfigProps, {}> {
                   cache.writeQuery({
                     query: GetNewsletterModalConfig,
                     data: {
-                      getNewsletterModalConfig: {
-                        modalEnabled: data.setNewsletterModalConfig.modalEnabled,
-                        __typename: data.setNewsletterModalConfig.__typename,
-                      },
+                      getNewsletterModalConfig: data.setNewsletterModalConfig,
                     },
                   })
                 }}
@@ -58,27 +84,58 @@ class NewsletterConfig extends React.Component<NewsletterConfigProps, {}> {
                   setNewsletterModalConfig,
                   { error: mutationError, loading: mutationLoading },
                 ) => {
-                  const enabled = data && data.getNewsletterModalConfig && data.getNewsletterModalConfig.modalEnabled
-                  return queryError || mutationError ? (
-                    <div>DEU ERRO</div>
-                  ) : (
-                    <div>NEWSLETTER = {enabled ? 'true' : 'false'}
-                      <br />
-                      <button onClick={() => {
-                        setNewsletterModalConfig({
-                          variables: {
-                            config: {
-                              modalTitle: '',
-                              modalDescription: '',
-                              modalTextSuccess: '',
-                              modalEnabled: !enabled,
-                              modalShowRule: '',
-                              modalShowWhen: '',
-                            },
-                          },
-                        })
-                      }}>CHANGE!</button>
-                    </div>
+                  {/* const enabled = data && data.getNewsletterModalConfig && data.getNewsletterModalConfig.modalEnabled */}
+                  return (
+                    <form onSubmit={(e) => this.handleSave(e, setNewsletterModalConfig)}>
+                      <Container isPlaceholderActive={queryLoading}>
+                        <span className="db g-mb4 g-f4 c-on-base">{this.props.intl.formatMessage({ id: 'newsletter-modal.admin.general' })}</span>
+                        <div className="mb4">
+                          <Input label="Label of input" className="w-100" />
+                        </div>
+                        <div className="mb4">
+                          <Input label="Label of input" className="w-100" />
+                        </div>
+                        <div className="mb5">
+                          <Input label="Label of input" className="w-100" />
+                        </div>
+                        <div className="mt2">
+                          <div className="dib">
+                            <label className="flex items-center">
+                              <Toggle value="isHuman" className="dib mr2" />
+                              Label
+                            </label>
+                          </div>
+                        </div>
+                      </Container>
+                      <Container isPlaceholderActive={queryLoading}>
+                        <span className="db g-mb4 g-f4 c-on-base">{this.props.intl.formatMessage({ id: 'newsletter-modal.admin.general' })}</span>
+                        <div className="mb4">
+                          <Input label="Label of input" className="w-100" />
+                        </div>
+                        <div className="mb4">
+                          <Input label="Label of input" className="w-100" />
+                        </div>
+                        <div className="mb5">
+                          <Input label="Label of input" className="w-100" />
+                        </div>
+                        <div className="mt2">
+                          <div className="dib">
+                            <label className="flex items-center">
+                              <Toggle value="isHuman" className="dib mr2" />
+                              Label
+                            </label>
+                          </div>
+                        </div>
+                      </Container>
+                      <div className={`flex justify-between items-center animated ${queryLoading ? 'o-0' : 'fadeIn'}`}>
+                        <Button onClick={this.handleCancel} style="secondary">
+                          {this.props.intl.formatMessage({ id: 'admin.container.cancel' })}
+                        </Button>
+                        <Button type="submit" style="primary">
+                          {this.props.intl.formatMessage({ id: 'admin.container.save' })}
+                        </Button>
+                      </div>
+                    </form>
                   )
                 }}
               </Mutation>
