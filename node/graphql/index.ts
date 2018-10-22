a446759b679cd69585aec67abc30f767f4ed9cd9
Gustavo Rosolem <guspace92@hotmail.com>
2018-10-22 13:58:07 -0300

    Newsletter modal first version working

--- node/graphql/index.ts
+++ node/graphql/index.ts
@@ -1,14 +1,15 @@
 import { loggerMiddleware } from '@gocommerce/utils'
 import { getNewsletterList } from './Query'
-import { unsubscribe } from './Mutation'
+import { unsubscribe, addNewsletterOmsProfile } from './Mutation'
 
 const tokenSplunk = 'dd433cc0-9106-4b0d-883a-377d57e8eb1a'
 
 export const resolvers = loggerMiddleware(tokenSplunk, {
   Query: {
-    getNewsletterList: async (_, param, ctx, info, makeApiCall) => await getNewsletterList(param, makeApiCall)
+    getNewsletterList: async (_, param, ctx, info, makeApiCall) => await getNewsletterList(param, makeApiCall),
   },
   Mutation: {
-    unsubscribe: async (_, param, ctx, info, makeApiCall) => await unsubscribe(param, makeApiCall)
+    unsubscribe: async (_, param, ctx, info, makeApiCall) => await unsubscribe(param, makeApiCall),
+    addNewsletterOmsProfile: (_, param, ctx, info, makeApiCall) => addNewsletterOmsProfile(param, ctx, makeApiCall),
   }
 })
