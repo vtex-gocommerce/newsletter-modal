8303d5ac30223c44c8b8a27e3fd5ecd37cd27edc
Augusto Lazaro <aemlazaro@gmail.com>
2019-04-15 14:32:23 -0300

    Fix: fix schema and add some type

--- node/graphql/index.ts
+++ node/graphql/index.ts
@@ -4,11 +4,27 @@ import { unsubscribe } from './Mutation'
 
 const tokenSplunk = 'dd433cc0-9106-4b0d-883a-377d57e8eb1a'
 
+export type Param = {
+  [key: string]: any,
+}
+
 export const resolvers = loggerMiddleware(tokenSplunk, {
   Query: {
-    getNewsletterList: async (_, param, ctx, info, makeApiCall) => await getNewsletterList(param, makeApiCall),
+    getNewsletterList: async (
+      _: any,
+      param: Param,
+      __: any,
+      ___: any,
+      makeApiCall: Function,
+    ) => await getNewsletterList(param, makeApiCall),
   },
   Mutation: {
-    unsubscribe: async (_, param, ctx, info, makeApiCall) => await unsubscribe(param, makeApiCall),
+    unsubscribe: async (
+      _: any,
+      param: { [key: string]: string | null },
+      __: any,
+      ___: any,
+      makeApiCall: Function,
+    ) => await unsubscribe(param, makeApiCall),
   }
 })
